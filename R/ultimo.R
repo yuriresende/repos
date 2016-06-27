9258beadc04518637c75eecc43d7f5446238df16
DLAB-LASA <hhh.gv.dlab@gmail.com>
2016-06-27 16:14:06 -0300

    Delete ultimo.R

--- R/ultimo.R
+++
@@ -1,9 +0,0 @@
-ultimo = function(x, na.rm = TRUE) {
-        if (all(is.na(x))) {
-            return(NA)
-        }
-        else {
-            aux = which(is.na(x) == FALSE)
-            return(x[aux[length(aux)]])
-        }
-        }
