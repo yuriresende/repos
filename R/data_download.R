e0b2a863d14cd611a03b2214ce9d3cb3609780de
DLAB-LASA <hhh.gv.dlab@gmail.com>
2016-10-20 15:39:00 -0200

    Update data_download.R

--- R/data_download.R
+++ R/data_download.R
@@ -389,5 +389,76 @@ data.download=function(variables=NULL,type="cia",SKUS=NULL, depart=c("D040")){
     cat("Data successfuly extracted. Processing time of", sqltime, "\n")
     return(tables)
   }
-  
+    if (type == "group-skus") {
+    t0 = Sys.time()
+    aux = c()
+    if (length(variables) > 1) {
+      for (i in 1:(length(variables) - 1)) {
+        aux = paste(aux, variables[i], ",")
+      }
+    }
+    aux = paste(aux, variables[length(variables)])
+    aux1 = c()
+    if (length(SKUS) > 1) {
+      for (i in 1:(length(SKUS) - 1)) {
+        aux1 = paste(aux1, SKUS[i], ",")
+      }
+    }
+    aux1 = paste(aux1, SKUS[length(SKUS)])
+    p1 = c(" SELECT GEO_CD_LOJA, to_char(PER_DT_DATA, 'yyyy-mm-dd') as PER_DT_DATA,")
+    p2 = c("FROM RPO_VDAEST_TRANSACIONAL WHERE PROD_CD_ITEM IN (")
+    p3 = c(") GROUP BY GEO_CD_LOJA, PER_DT_DATA")
+    sql = paste(p1, aux, p2, aux1, p3)
+    query = dbSendQuery(con, paste(sql))
+    query = fetch(query, n = -1)
+    taux = Sys.time()
+    cat("time elapsed:", taux - t0, "- Data successfuly downloaded from Oracle, organizing into tables...", 
+        "\n")
+    dates = as.Date(dates)
+    dates = seq(from = dates[1], to = dates[2], by = "days")
+    dates = as.character(dates)
+    tabloj <- dbSendQuery(con, paste("SELECT * FROM puc.RPO_CADASTRO_LOJA"))
+    tabloj = fetch(tabloj, n = -1)
+    loj = unique(tabloj$GEO_CD_LOJA)
+    table = matrix(NA, length(dates), length(loj)) 
+
+    dimnames(table) = list(dates, loj)
+    range = 1:nrow(query)
+    gcfv = function(q, dates, loj) {
+      id1 = which(dates == q[2])
+      id2 = which(loj == q[1])
+      res = c(id1, id2)
+      return(res)
+    }
+    n.cores = detectCores() - 1
+    aux = cbind(query[, 1], query[, 2])
+    cl = makeCluster(n.cores)
+    indexes = parApply(cl, aux, 1, gcfv, dates = dates, loj = loj)
+    stopCluster(cl)
+    cat("just a couple more minutes =)", "\n")
+
+    te = function(q, table, range, indexes) {
+      for (i in range) {
+        table[indexes[1, i], indexes[2, i]] = q[i]
+      }
+      return(table)
+    }
+
+    aux = list()
+    for (i in 1:length(variables)) {
+      aux[[i]] = query[, i + 2]
+    }
+    n.cores = min(length(variables), 32)
+    cl = makeCluster(n.cores)
+    tables = parLapply(cl, aux, te, range = range, table = table, 
+                       indexes = indexes)
+    names(tables) = variables
+    stopCluster(cl)
+    t1 = Sys.time()
+    sqltime = (t1 - t0)
+    cat("Data successfuly extracted. Processing time of", 
+        sqltime, "\n")
+    return(tables)
+  }
+
 }
