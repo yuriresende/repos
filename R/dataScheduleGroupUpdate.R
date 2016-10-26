898da7f9492bc9392d445bf20d4b518098169963
DLAB-LASA <hhh.gv.dlab@gmail.com>
2016-10-26 15:36:38 -0200

    Create dataScheduleGroupUpdate.R

---
+++ R/dataScheduleGroupUpdate.R
@@ -0,0 +1,17 @@
+dataScheduleGroupUpdate=function(when="sexta 20 00"){
+  
+  loop=10
+  while(loop==10){
+    
+    check=Sys.time()
+    day=weekdays(check)
+    hora=hour(check)
+    min=minute(check)
+    check=paste(day,hora,min)
+    
+    if(check==when){
+      save.base=dataGroupBaseUpdate()
+    }
+    
+  }
+}
