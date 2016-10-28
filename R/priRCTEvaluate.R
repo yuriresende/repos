f54697abe84931bfbb8539d805e55588e2971fba
DLAB-LASA <hhh.gv.dlab@gmail.com>
2016-10-28 10:40:01 -0200

    Update priRCTEvaluate.R

--- R/priRCTEvaluate.R
+++ R/priRCTEvaluate.R
@@ -1,24 +1,24 @@
-priRCTEvaluate<-function(data.painel, dias.tratamento, familia){
+priRCTEvaluate<-function(data.painel, dias.tratamento, SKUGroup){
   
   ###############################################################
   # Modelo Linear com efeitos fixos de loja e controle por dia
   ###############################################################
   
-  col_p_medio<-which(colnames(data.painel)==paste('p.medio',familia,sep='.'))
-  col_vda_qtd<-which(colnames(data.painel)==paste('VDA_QTD_SKU',familia,sep=''))
+  col_p_medio<-which(colnames(data.painel)==paste('p_medio_G',SKUGroup,sep=''))
+  col_vda_qtd<-which(colnames(data.painel)==paste('VDA_QTD_G',SKUGroup,sep=''))
   
-  dummy_dia<-model.matrix(~as.factor(data.painel$data)-1)
+  dummy_dia<-model.matrix(~as.factor(data.painel$Data)-1)
   dummy_dia<-dummy_dia[,-1]
   
-  fit1 <- plm(data.painel[,col_vda_qtd] ~ data.painel[,'P_SAP'] + dummy_dia, data = data.painel, index=c("loja"), effect="individual", model="within")
+  fit1 <- plm(data.painel[,col_vda_qtd] ~ data.painel[,'P_SAP'] + dummy_dia, data = data.painel, index=c("Loja"), effect="individual", model="within")
   
-  fit_iv <- plm(data.painel[,col_p_medio] ~ data.painel[,'P_SAP'] + dummy_dia, data = data.painel, index=c('loja'), effect='individual', model='within')
+  fit_iv <- plm(data.painel[,col_p_medio] ~ data.painel[,'P_SAP'] + dummy_dia, data = data.painel, index=c('Loja'), effect='individual', model='within')
   
   fitted <- data.painel[,col_p_medio] - fit_iv$residuals
   
-  fit2 <- plm(data.painel[,col_vda_qtd] ~ fitted + dummy_dia, data = data.painel, index=c("loja"), effect="individual",model="within")
+  fit2 <- plm(data.painel[,col_vda_qtd] ~ fitted + dummy_dia, data = data.painel, index=c("Loja"), effect="individual",model="within")
   
-  fit_did <- plm(data.painel[,col_vda_qtd] ~ data.painel[,'D1'] + data.painel[,'D2'] + data.painel[,'D3'] + dummy_dia, data = data.painel, index=c("loja"), effect="individual", model="within")
+  fit_did <- plm(data.painel[,col_vda_qtd] ~ data.painel[,'D1'] + data.painel[,'D2'] + data.painel[,'D3'] + dummy_dia, data = data.painel, index=c("Loja"), effect="individual", model="within")
   
   ###############################
   # Tabelas para OUTPUT
@@ -47,11 +47,11 @@ priRCTEvaluate<-function(data.painel, dias.tratamento, familia){
   # Calculo do coeficiente agregado, está usando modelo com IV
   beta = fit2$coefficients[1]
   
-  b = beta*length(unique(data.painel$loja))*dias.tratamento
+  b = beta*length(unique(data.painel$Loja))*dias.tratamento
   
   # Número de empresas em tratamento ou controle
-  nTrat = length(unique(data.painel$loja[which(data.painel$D1 == 1)]))
-  nCont = length(unique(data.painel$loja)) - nTrat
+  nTrat = length(unique(data.painel$Loja[which(data.painel$D1 == 1)]))
+  nCont = length(unique(data.painel$Loja)) - nTrat
   
   # Quantidade total (Q_tratamento + Q_controle) vendida durante o período de tratamento
   Q = sum(data.painel[which(data.painel$D2 == 1), col_vda_qtd])
